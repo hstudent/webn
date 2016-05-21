21cdd3c4b3356830f8bb11d5fcd18bc8a317cf5c
hstudent <humpt_tj@mail.ru>
2016-05-21 21:22:49 +0100

    models modified

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=256)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add = True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, null = True)
+	likes = models.ManyToManyField(User, related_name = 'qlikes')
+        class Meta:
+                db_table = 'question'
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add = True)
+	question = models.ForeignKey(Question, null = True)
+	author = models.ForeignKey(User, null = True)
+        class Meta:
+                db_table = 'answer'
+
